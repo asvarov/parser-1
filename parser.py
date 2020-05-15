505c56f0a4aa5b969131f896f907056aeec4485c
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-05-15 20:40:46 +0300

    fix csv writer

--- parser.py
+++ parser.py
@@ -16,6 +16,7 @@ def write_txt(url):
     text_original, text_translate, name = parse(url)
     with open("%s.txt" % name, "w", newline='', encoding='utf-8') as txt_file:
         leng = len(text_original)
+        print(range(leng))
         for i in range(leng):
             txt_file.write(text_original[i])
             txt_file.write(text_translate[i])
@@ -26,9 +27,11 @@ def write_csv(url):
     with open("%s.csv" % name, "w", newline='', encoding='utf-8') as csv_file:
         leng = len(text_original)
         write = csv.writer(csv_file)
+        tmp = []
         for i in range(leng):
-            write.writerow(text_original[i])
-            write.writerow(text_translate[i])
+            tmp.append(text_original[i])
+            tmp.append(text_translate[i])
+        write.writerow(tmp)
 
 
 def get_name(url):
@@ -39,7 +42,6 @@ def main():
     url = "https://www.amalgama-lab.com/songs/i/i_am_giant/death_of_you.html"
     write_txt(url)
     write_csv(url)
-
 
 if __name__ == "__main__":
     main()
