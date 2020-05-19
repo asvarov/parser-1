c5f0a1d47e3c7dcb0ac461b6d517944c9e320686
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-05-19 18:49:47 +0300

    add list of domains ,fix error, add timeit

--- api_domain_info.py
+++ api_domain_info.py
@@ -9,53 +9,64 @@ https://www.fl.ru/projects/4395023/dlya-spiska-domenov-opredelit-datu-registrats
 
 url = 'http://api.whois.vu/'
 date_format = "%Y-%m-%d"
-params = {'q': 'kvvp.com.ua'}
 
 
-def calculate(params):
-    request = requests.get(url, params=params)
-    response = request.json()
-    if request.status_code == 200:
-        domains = []
-        domain = response["domain"]
-    #    available = response["available"]
-    #    country = response["type"]
-    #    status = response["statuses"][0]
-        created_epoch = response["created"]
-    #    updated_epoch = response["updated"]
-    #    expires_epoch = response["expires"]
-        created = datetime.datetime.fromtimestamp(created_epoch).strftime(date_format)
-    #    updated = datetime.datetime.fromtimestamp(updated_epoch).strftime(date_format)
-    #    expired = datetime.datetime.fromtimestamp(expires_epoch).strftime(date_format)
-        today_date = datetime.datetime.fromtimestamp(time.time()).strftime(date_format)
-        count_days = datetime.datetime.strptime(today_date, date_format) - datetime.datetime.strptime(created, date_format)
-    #    print('Today date:', today_date)
-    #    print('Domain name:', domain)
-    #    print('Domain available to registration:', available)
-    #    print('Domain registered in:', country)
-    #    print('Domain status:', status)
-    #    print('Domain created:', created)
-    #    print('Domain updated:', updated)
-    #    print('Domain expired:', expired)
-    #    print('Domain registered', count_days.days,  'days ago!')
-        domains.append({'domain': domain, 'registered': created, 'days_ago': count_days.days})
-        print(domains)
-        return domains
-
-    else:
-        print('Error!')
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
 
 
-def write_csv(domains):
+@timeit
+def readfile():
+    with open('domains.txt') as f:
+        domains = f.read().splitlines()
+        domains_list = []
+        for i in domains:
+            domains_list.append({'q': i})
+    return domains_list
+
+
+@timeit
+def calculate():
+    params = readfile()
+    domains = []
+    today_date = datetime.datetime.fromtimestamp(time.time()).strftime(date_format)
+    for param in params:
+        request = requests.get(url, params=param)
+        response = request.json()
+        if request.status_code == 200:
+            domain = response["domain"]
+            created_epoch = response["created"]
+            created = datetime.datetime.fromtimestamp(created_epoch).strftime(date_format)
+            count_days = datetime.datetime.strptime(today_date, date_format) - datetime.datetime.strptime(created, date_format)
+            domains.append({'domain': domain, 'registered': created, 'days_ago': count_days.days})
+        else:
+            print('Error!')
+    return domains
+
+
+@timeit
+def write_csv():
+    domains = calculate()
     with open("domains.csv", "w", newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        writer.writerow(['Domian name', 'Registered date', 'Days ago'])
+        writer.writerow(['Domain name', 'Registered date', 'Days ago'])
         for item in domains:
             writer.writerow([item['domain'], item['registered'], item['days_ago']])
 
 
 def main():
-    write_csv(calculate(params))
+    write_csv()
+
 
 if __name__ == "__main__":
     main()
