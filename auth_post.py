5da3a2280128ce74859855cf46927ea7c2a69878
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-05-21 01:45:19 +0300

    fix get after post auth

--- auth_post.py
+++ auth_post.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from account import login, password, email, emailpwd
+from account import login, password, email, emailpwd, name, pwd
 
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
@@ -18,20 +18,20 @@ def goodgame(login, password):
     return request
 
 
-def alpkr(login, password):
+def alpkr(name, pwd):
     session = requests.Session()
-
-    data = {'name': login,
-            'pass': password,
+    data = {'name': name,
+            'pass': pwd,
             'op': 'Вход',
-            'form_build_id': 'form - Rmtjw_g3uOT9_2wFxoatFlFmsBECyJxU4XXMgXtT - Ak',
+            'form_build_id': 'form-HpxAZbwtVPqnAKt9aRouPY-DNU-5OP2gYUzN3AXpuro',
             'form_id': 'user_login_block',
     }
-    request = session.post("http://alpkr.dp.ua/?q=users/" + login, headers=HEADERS, data=data)
-    return request
+    session.post("http://alpkr.dp.ua/?q=node&destination=node", headers=HEADERS, data=data)
+    response = session.get("http://alpkr.dp.ua/?q=admin/content/node")
+    return response
 
 
-def olx(login, password):
+def olx(email, emailpwd):
     session = requests.Session()
     data = {'login[email_phone]': email,
             'login[password]': emailpwd,
@@ -41,14 +41,17 @@ def olx(login, password):
     request = session.post("https://www.olx.ua/account/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=index", headers=HEADERS, data=data)
     return request
 
+
 if __name__ == '__main__':
 
-    req1 = goodgame(login, password)
-    req2 = alpkr(login, password)
-    req3 = olx(login, password)
-    req1json = json.loads(req1.text)
-    print(req1)
-    print(req2.text)
-    print(req1json['response'])
-    print(req3.text)
+
+#    req1 = goodgame(login, password)
+#    req2 = alpkr(login, password)
+    request = alpkr(name, pwd)
+#    req3 = olx(email, emailpwd)
+#    req1json = json.loads(req1.text)
+#    print(req1)
+    print(request.text)
+#    print(req1json['response'])
+#    print(req3.text)
 
