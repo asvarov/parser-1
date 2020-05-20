8d3a7a7fcf7cb378800868b5a21182f258d8b655
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-05-20 22:50:33 +0300

    divide code to 3 defs

--- auth_post.py
+++ auth_post.py
@@ -6,43 +6,49 @@ HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
 
 
-def loginbot(login, password):
-    session1 = requests.Session()
-    session2 = requests.Session()
-    session3 = requests.Session()
-
-    data1 = {'login': login,
+def goodgame(login, password):
+    session = requests.Session()
+    data = {'login': login,
             'password': password,
             'remember': '1',
             'return': 'user',
             'backurl': '',
     }
+    request = session.post("https://goodgame.ru/ajax/login/", headers=HEADERS, data=data)
+    return request
 
-    data2 = {'name': login,
+
+def alpkr(login, password):
+    session = requests.Session()
+
+    data = {'name': login,
             'pass': password,
             'op': 'Вход',
             'form_build_id': 'form - Rmtjw_g3uOT9_2wFxoatFlFmsBECyJxU4XXMgXtT - Ak',
             'form_id': 'user_login_block',
     }
-    data3 = {'login[email_phone]': email,
+    request = session.post("http://alpkr.dp.ua/?q=users/" + login, headers=HEADERS, data=data)
+    return request
+
+
+def olx(login, password):
+    session = requests.Session()
+    data = {'login[email_phone]': email,
             'login[password]': emailpwd,
             'login[zxcvbn - password - strength]': '0.0000100001',
             'login[zxcvbn - score - strength]': '1',
     }
-
-
-    request1 = session1.post("https://goodgame.ru/ajax/login/", headers=HEADERS, data=data1)
-    request2 = session2.post("http://alpkr.dp.ua/?q=users/" + login, headers=HEADERS, data=data2)
-    request3 = session2.post("https://www.olx.ua/account/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=index", headers=HEADERS, data=data3)
-    return request1, request2, request3
+    request = session.post("https://www.olx.ua/account/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=index", headers=HEADERS, data=data)
+    return request
 
 if __name__ == '__main__':
 
-    req1, req2, req3 = loginbot(login, password)
-#    req2json = json.loads(req.text)
+    req1 = goodgame(login, password)
+    req2 = alpkr(login, password)
+    req3 = olx(login, password)
+    req1json = json.loads(req1.text)
     print(req1)
-    print(req2)
+    print(req2.text)
+    print(req1json['response'])
     print(req3.text)
-#    print(req2json)
-#    print(req2json['response'])
 
