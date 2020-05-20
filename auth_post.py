1f2b3995b824c4d13dc02b9ed24a8b10a8ec6ba8
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-05-20 15:26:32 +0300

    post login to random site

--- auth_post.py
+++ auth_post.py
@@ -1,5 +1,21 @@
+import json
 import requests
 
-def loginbot():
-    r = requests.get('https://goodgame.ru/forum')
-    print(r.text)
+def loginbot(login, password):
+    session = requests.Session()
+    session.get("https://goodgame.ru/forum")
+    data = {'login': login,
+            'password': password,
+            'remember': '1',
+            'return': 'user',
+            'backurl': '',
+    }
+    request = session.post("https://goodgame.ru/ajax/login/", data=data)
+    return request.text
+
+if __name__ == '__main__':
+
+    req = loginbot('login', 'password')
+    req2json = json.loads(req)
+    print(req2json['response'])
+
