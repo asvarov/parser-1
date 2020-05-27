64c1aafc73a97a2efe5556f2a0fdca444bc0dc15
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-05-27 12:25:14 +0300

    create modules to download files to folder

---
+++ download_files.py
@@ -0,0 +1,38 @@
+import requests
+import os
+
+urls = ['https://images.pexels.com/photos/853199/pexels-photo-853199.jpeg',
+        'https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg',
+        'https://images.pexels.com/photos/1213447/pexels-photo-1213447.jpeg'
+]
+
+
+def get_file(url):
+    r = requests.get(url, stream=True)
+    return r
+
+
+def get_name(url):
+    name = url.split('/')[-1]
+    folder = url.split('/')[3]
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    path = os.path.abspath(folder)
+
+    return path + '\\' + name
+
+
+def save_image(name, file_object):
+    with open(name, 'bw') as f:
+        for chung in file_object.iter_content(8192):
+            f.write(chung)
+
+
+def main():
+    for url in urls:
+        save_image(get_name(url), get_file(url ))
+
+
+if __name__ == '__main__':
+    main()
