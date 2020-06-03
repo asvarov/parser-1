ff211f5ef8a5c6a7c0d1b2d4ef2e1d9e65ec3a08
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-06-03 10:23:01 +0300

    try realise it by BS4

--- xml_to_json.py
+++ xml_to_json.py
@@ -1,14 +1,37 @@
-import xmltodict
-import json
-
+# from xml.dom.minidom import parseString
+#
+# import dicttoxml
+# import xmltodict
+# import json
+#
+#
 source = 'source.xml'
-result = 'result.json'
+# result = 'result.json'
+# reverse = 'reverse.xml'
+#
+# with open(source, 'r') as f:
+#     doc = xmltodict.parse(f.read())
+# with open(result, 'w') as f:
+#     json.dump(doc, f, ensure_ascii=False, indent=4)
+#
+# # import pprint
+# # pp.pprint(json.dumps(doc))
+# # pp = pprint.PrettyPrinter(indent=4)
+#
+# with open(result, 'r') as f:
+#     doc = json.loads(f.read())
+#
+# pretty = parseString(dicttoxml.dicttoxml(doc, root=False, attr_type=False))
+#
+# with open(reverse, 'w') as f:
+#     f.write(pretty.toprettyxml(indent=' '*4))
+#
 
-with open(source, 'r') as f:
-    doc = xmltodict.parse(f.read())
-with open(result, 'w') as f:
-    json.dump(doc, f, ensure_ascii=False, indent=4)
+from bs4 import BeautifulSoup as bs
 
-# import pprint
-# pp.pprint(json.dumps(doc))
-# pp = pprint.PrettyPrinter(indent=4)
+content = []
+with open(source, "r") as file:
+    content = file.readlines()
+    content = "".join(content)
+    bs_content = bs(content, "lxml")
+    print(bs_content)
