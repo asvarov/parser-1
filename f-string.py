5fa02a53977f401d1600326b24551a2e61408ee7
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-06-10 23:21:29 +0300

    f-string added uses

--- f-string.py
+++ f-string.py
@@ -12,3 +12,18 @@ person = [
 for name, lastname, balance, gender in person:
     message = f'{start[gender]} {name} {lastname}, на вашем счету отсталось {balance} грн.'
     print(message)
+
+print(f'33*44={33 * 44}')
+
+for name, lastname, balance, gender in person:
+    message = f'{name=} {lastname=} {balance=}'
+    print(message)
+
+a = 45.76589045
+print(f'{a:.2f}')
+b = 1234567890
+print(f'{b:,}')
+
+total = 567
+part = 432
+print(f'Percentage: {part/total:.2%}')
--- youtube_download.py
+++ youtube_download.py
@@ -4,5 +4,10 @@ video = 'https://www.youtube.com/watch?v=sXQxhojSdZM'
 playlist = 'https://www.youtube.com/watch?v=cdTe3ec2MdM&list=PLve9Z4J4Fbojj-Oy2ll1x-GNynMRx_56I'
 
 ytfd = YouTube(video)
-print(ytfd.title)
-
+ytpd = YouTube(playlist)
+stream_video = ytfd.streams.first()
+# stream_video.download()
+stream_audio = ytfd.streams.filter(only_audio=True).first()
+stream_audio.download()
+stream_playlist = ytpd.streams.all
+stream_playlist.download
