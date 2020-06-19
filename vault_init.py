ce103216af79c84987f1325d3db647b0f288ec95
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-06-19 17:49:49 +0300

    hasicorp vault optimized code, delete prints

--- vault_init.py
+++ vault_init.py
@@ -1,41 +1,33 @@
 import hvac
 
+
 client = hvac.Client(url='http://192.168.10.10:8200')
-client.sys.is_initialized()
-# False
+shares = 5
+threshold = 3
+
 if client.sys.is_sealed() is True and client.sys.is_initialized() is False:
-    shares = 5
-    threshold = 3
 
     result = client.sys.initialize(shares, threshold)
     root_token = result['root_token']
-    print(f'root token: {root_token}')
+    keys = result['keys']
+    client.token = root_token
+
     with open('root_token_vault.json', mode='w') as f:
         f.write(root_token)
 
-    keys = result['keys']
     for i in keys:
-        with open(f'individual_key_{keys.index(i)+1}_vault.json', mode='w') as f:
+        with open(f'individual_key_{keys.index(i) + 1}_vault.json', mode='w') as f:
             f.write(i)
-    client.sys.is_initialized()
-    # True
-    client.token = root_token
 
-    print(f'Storage is sealed?: {client.sys.is_sealed()}') # True
-    # unseal with individual keys
+    print(f'Storage is sealed?: {client.sys.is_sealed()}')
+
     for i in keys:
-        print(f'individual key #{keys.index(i)+1}: {keys[keys.index(i)]}')
+        print(f'individual key #{keys.index(i) + 1}: {keys[keys.index(i)]}')
 
     for i in range(threshold):
-        # Unseal a Vault cluster with individual keys
         unseal_response = client.sys.submit_unseal_key(keys[i])
 
-    client.sys.seal()
-    # <Response [204]>
-    # Unseal with multiple keys until threshold met
     unseal_response = client.sys.submit_unseal_keys(keys)
     print(f'Storage is sealed?: {client.sys.is_sealed()}')
-    # False
 else:
-    print(f'Storage is unsealed and client is_initialized!')
-
+    print(f'Storage is unsealed and client is initialized!')
