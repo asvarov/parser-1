f2ef2b63f7126e419ac6d5c7d133a5b413ca205e
Maksym Asvarov <asvarov.maxim@gmail.com>
2020-06-22 10:37:45 +0300

    hasicorp vault create kv path

--- vault_write_read_kv.py
+++ vault_write_read_kv.py
@@ -1,11 +1,26 @@
 import hvac
 
+with open('root_token_vault.json', mode='r') as f:
+    root_token = f.read()
 
-client = hvac.Client(url='http://192.168.10.10:8200', token='s.3F3ksyYxNo5J1iyEohZuADFC')
+client = hvac.Client(url='http://192.168.10.10:8200', token=root_token)
 r = client.is_authenticated()
-print(r)
-print(client.sys.is_sealed())
-#
+print(f'Client is authenticated: {r}')
+
+secrets_engines_list = client.sys.list_mounted_secrets_engines()['data']
+
+try:
+    client.sys.enable_secrets_engine(
+        backend_type='kv',
+        path='hvac-kv',
+    )
+except hvac.exceptions.InvalidRequest as error:
+    print(f'Creation {error} - skipped.')
+
+print(secrets_engines_list.keys())
+print('The following secrets engines are mounted: %s' % ', '.join(sorted(secrets_engines_list.keys())))
+
+
 # client.secrets.kv.v2.configure(max_versions=20, mount_point='secret',)
 # client.secrets.kv.v2.create_or_update_secret(path='pythonapp', secret=dict(login='username', password='userpassword'),)
 #
@@ -14,7 +29,7 @@ print(client.sys.is_sealed())
 # password = read_response['data']['data']['password']
 # print(f'Value under path "secret/pythonapp" / key "login": {login}')
 # print(f'Value under path "secret/pythonapp" / key "password": {password}')
-#
+
 # list_response = client.secrets.kv.v2.list_secrets(
 #     path='pythonapp/login',
 # )
